45e3d5458e09b7b5b90f133d1479b2491dcf3578
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-25 15:29:20 -0300

    r1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        count_student = 0
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                count_student += 1
+        return count_student
+    except TypeError:
+        return None
