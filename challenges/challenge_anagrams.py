11fd7d22ae749dd3664dadfb8d5defc85a063a49
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-25 18:45:05 -0300

    dps vejo isso... req 4 falhando

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,21 +1,54 @@
-def string_dict(string):
-    string_dict = {}
-    for char in string.lower():
-        if char in string_dict:
-            string_dict[char] += 1
+def is_anagram(first_string: str, second_string: str) -> tuple:
+
+    # Ver strings estão vazias
+    if not first_string and not second_string:
+        return ("", "", True)
+    """Retorna uma tupla contendo duas strings ordenadas e
+    um booleano"""
+    if not isinstance(first_string, str) or not isinstance(second_string, str):
+        raise TypeError("As entradas devem ser strings.")
+
+    first_list = list(first_string.lower())
+    second_list = list(second_string.lower())
+    _sort_list(first_list)
+    _sort_list(second_list)
+    first_sorted = "".join(first_list)
+    second_sorted = "".join(second_list)
+    return (first_sorted, second_sorted, first_sorted == second_sorted)
+
+
+def _sort_list(input_list: list, start: int = 0, end: int = None) -> None:
+    """Ordena a lista em ordem crescente, usando o algoritmo mergesort."""
+    if end is None:
+        end = len(input_list)
+    if (end - start) > 1:
+        middle_index = (start + end) // 2
+        _sort_list(input_list, start, middle_index)
+        _sort_list(input_list, middle_index, end)
+        _merge_lists(input_list, start, middle_index, end)
+
+
+def _merge_lists(
+        input_list: list, start: int, middle_index: int, end: int) -> None:
+    """Combina duas listas ordenadas em uma única lista ordenada."""
+    i = start
+    j = middle_index
+    aux = []
+
+    while i < middle_index and j < end:
+        if input_list[i] < input_list[j]:
+            aux.append(input_list[i])
+            i += 1
         else:
-            string_dict[char] = 1
-    return string_dict
+            aux.append(input_list[j])
+            j += 1
 
+    while i < middle_index:
+        aux.append(input_list[i])
+        i += 1
 
-def is_anagram(first_string, second_string):
-    if len(first_string) != len(second_string):
-        return False
-    first_string_dict = string_dict(first_string)
-    second_string_dict = string_dict(second_string)
-    for char in first_string_dict:
-        if char not in second_string_dict:
-            return False
-        if first_string_dict[char] != second_string_dict[char]:
-            return False
-    return True
+    while j < end:
+        aux.append(input_list[j])
+        j += 1
+
+    input_list[start:end] = aux
